888eee8a28e0f1d2c1fdaa34041802fd78dfcf3d
Ahmad Belbeisi <35712320+ahmadbelb@users.noreply.github.com>
2021-11-14 18:19:56 +0100

    sdf

---
+++ docs/source/Plot/create_sections 2.m
@@ -0,0 +1,190 @@
+% Function
+function [mesh_list, y_values] = create_sections(F,V,N,number_of_sections_or_vector)
+%SPGL1  Solve basis pursuit, basis pursuit denoise, and LASSO
+%
+% [x, r, g, info] = spgl1(A, b, tau, sigma, x0, options)
+% [x, r, g, info] = spgl1(A, b, tau [, sigma, x0] [,options])
+%
+% ---------------------------------------------------------------------
+% Solve the basis pursuit denoise (BPDN) problem
+%
+% (BPDN)   minimize  ||x||_1  subj to  ||Ax-b||_2 <= sigma,
+%
+% or the l1-regularized least-squares problem
+%
+% (LASSO)  minimize  ||Ax-b||_2  subj to  ||x||_1 <= tau.
+%
+% Setting the optional parameter mu uses updated A and b:
+%
+% A = [A        ] and b = [b]
+%     [sqrt(mu)I] and     [0].
+% ---------------------------------------------------------------------
+%
+% INPUTS
+% ======
+% A        is an m-by-n matrix, explicit or an operator.
+%          If A is a function, then it must have the signature
+%
+%          y = A(x,mode)   if mode == 1 then y = A x  (y is m-by-1);
+%                          if mode == 2 then y = A'x  (y is n-by-1).
+%
+% b        is an m-vector.
+% tau      is a nonnegative scalar; see (LASSO).
+% sigma    if sigma != inf or != [], then spgl1 will launch into a
+%          root-finding mode to find the tau above that solves (BPDN).
+%          In this case, it's STRONGLY recommended that tau = 0.
+% x0       is an n-vector estimate of the solution (possibly all
+%          zeros). If x0 = [], then SPGL1 determines the length n via
+%          n = length( A'b ) and sets  x0 = zeros(n,1).
+% options  is a structure of options from spgSetParms. Any unset options
+%          are set to their default value; set options=[] to use all
+%          default values.
+%
+% OUTPUTS
+% =======
+% x        is a solution of the problem
+% r        is the residual, r = b - Ax
+% g        is the gradient, g = -A'r
+% info     is a structure with the following information:
+%          .tau     final value of tau (see sigma above)
+%          .rNorm   two-norm of the optimal residual
+%          .rGap    relative duality gap (an optimality measure)
+%          .gNorm   Lagrange multiplier of (LASSO)
+%          .stat    = 1 found a BPDN solution
+%                   = 2 found a BP sol'n; exit based on small gradient
+%                   = 3 found a BP sol'n; exit based on small residual
+%                   = 4 found a LASSO solution
+%                   = 5 error: too many iterations
+%                   = 6 error: linesearch failed
+%                   = 7 error: found suboptimal BP solution
+%                   = 8 error: too many matrix-vector products
+%          .time    total solution time (seconds)
+%          .nProdA  number of multiplications with A
+%          .nProdAt number of multiplications with A'
+%
+% OPTIONS
+% =======
+% Use the options structure to control various aspects of the algorithm:
+%
+% options.fid         File ID to direct log output
+%        .verbosity   0=quiet, 1=some output, 2=more output.
+%        .iterations  Max. number of iterations (default if 10*m).
+%        .bpTol       Tolerance for identifying a basis pursuit solution.
+%        .optTol      Optimality tolerance (default is 1e-4).
+%        .decTol      Larger decTol means more frequent Newton updates.
+%        .subspaceMin 0=no subspace minimization, 1=subspace minimization.
+%
+% EXAMPLE
+% =======
+%   m = 120; n = 512; k = 20; % m rows, n cols, k nonzeros.
+%   p = randperm(n); x0 = zeros(n,1); x0(p(1:k)) = sign(randn(k,1));
+%   A  = randn(m,n); [Q,R] = qr(A',0);  A = Q';
+%   b  = A*x0 + 0.005 * randn(m,1);
+%   opts = spgSetParms('optTol',1e-4);
+%   [x,r,g,info] = spgl1(A, b, 0, 1e-3, [], opts); % Find BP sol'n.
+%
+% AUTHORS
+% =======
+%  Ewout van den Berg (vandenberg.ewout@gmail.com)
+%  Michael P. Friedlander (mpf@cs.ubc.ca)
+%
+% BUGS
+% ====
+% Please send bug reports or comments to
+%            Michael P. Friedlander (mpf@cs.ubc.ca)
+%            Ewout van den Berg (vandenberg.ewout@gmail.com)
+
+% 15 Apr 07: First version derived from spg.m.
+% 17 Apr 07: Added root-finding code.
+% 18 Apr 07: sigma was being compared to 1/2 r'r, rather than
+%            norm(r), as advertised.  Now immediately change sigma to
+%            (1/2)sigma^2, and changed log output accordingly.
+% 24 Apr 07: Added quadratic root-finding code as an option.
+% 24 Apr 07: Exit conditions need to guard against small ||r||
+%            (ie, a BP solution).  Added test1,test2,test3 below.
+% 15 May 07: Trigger to update tau is now based on relative difference
+%            in objective between consecutive iterations.
+% 15 Jul 07: Added code to allow a limited number of line-search
+%            errors.
+% 23 Feb 08: Fixed bug in one-norm projection using weights. Thanks
+%            to Xiangrui Meng for reporting this bug.
+% 26 May 08: The simple call spgl1(A,b) now solves (BPDN) with sigma=0.
+% 18 Mar 13: Reset f = fOld if curvilinear line-search fails.
+%            Avoid computing the Barzilai-Borwein scaling parameter
+%            when both line-search algorithms failed.
+% 09 Sep 13: Recompute function information at new x when tau decreases.
+%            Fixed bug in subspace minimization. Thanks to Yang Lei
+%            for reporting this bug.
+
+
+%   ----------------------------------------------------------------------
+%   This file is part of SPGL1 (Spectral Projected-Gradient for L1).
+%
+%   Copyright (C) 2007 Ewout van den Berg and Michael P. Friedlander,
+%   Department of Computer Science, University of British Columbia, Canada.
+%   All rights reserved. E-mail: <{ewout78,mpf}@cs.ubc.ca>.
+%
+%   SPGL1 is free software; you can redistribute it and/or modify it
+%   under the terms of the GNU Lesser General Public License as
+%   published by the Free Software Foundation; either version 2.1 of the
+%   License, or (at your option) any later version.
+%
+%   SPGL1 is distributed in the hope that it will be useful, but WITHOUT
+%   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+%   or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General
+%   Public License for more details.
+%
+%   You should have received a copy of the GNU Lesser General Public
+%   License along with SPGL1; if not, write to the Free Software
+%   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301
+%   USA
+%   ----------------------------------------------------------------------
+
+
+if(length(number_of_sections_or_vector)==1)
+    YminGEO=min(V(:,2));
+    YmaxGEO=max(V(:,2));
+    delta=(YmaxGEO-YminGEO)/(number_of_sections_or_vector);
+
+    YcutGEO=zeros(number_of_sections_or_vector+1,1);
+    for r=1:number_of_sections_or_vector+1%parfor r=1:number_of_sections+1
+        YcutGEO(r)=YminGEO+delta*(r-1);
+    end
+else
+    YcutGEO=number_of_sections_or_vector;
+end
+% YcutGEO = [-365,-270,-211,-165,-125,-110,30,211,365];%%%%%%%%%%%
+y_values=YcutGEO;
+mesh_list = cell(length(YcutGEO)-1,3);
+
+
+for i=1:(length(YcutGEO)-2)
+    
+    [FGR,VGR,NGR,FRD,VRD,NRD]=cut_the_geometry(F,V,N,YcutGEO(i+1));
+
+    mesh_list{i,1}=FGR;
+    mesh_list{i,2}=VGR;
+    mesh_list{i,3}=NGR;
+
+    F=FRD;
+    V=VRD;
+    N=NRD;
+
+end
+% The rightmost mesh_list is the remaining mesh, after all cuttings
+mesh_list{end,1}=F;
+mesh_list{end,2}=V;
+mesh_list{end,3}=N;
+
+% for i=1:(length(y_values)-1)
+%     figure();
+%     plot_STL(mesh_list{i,2},mesh_list{i,1});
+%     axis equal;
+% end
+
+
+end
+
+ 
+
+
