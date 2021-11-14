30b6be3172c7c2b407092039d67afb9067a1f57e
Ahmadbelbeisi <80634422+Ahmadbelbeisi@users.noreply.github.com>
2021-11-14 22:37:56 +0100

    OS

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,7 @@
 import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
-
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 # -- Project information -----------------------------------------------------
 
