516b075647baa8250113cf32ebaec034533dc373
Ahmadbelbeisi <80634422+Ahmadbelbeisi@users.noreply.github.com>
2021-11-14 22:44:01 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -134,57 +134,6 @@ html_css_files = [
 htmlhelp_basename = 'sphinxcontrib-matlabdomaindoc'
 
 
-# -- Options for LaTeX output ------------------------------------------------
-
-latex_elements = {
-    # The paper size ('letterpaper' or 'a4paper').
-    'papersize': 'letterpaper',
-    # The font size ('10pt', '11pt' or '12pt').
-    'pointsize': '10pt',
-    # Additional stuff for the LaTeX preamble.
-    #'preamble': '',
-    'maxlistdepth' : '12',
-    'fontpkg': r'''
-\usepackage[scaled]{helvet} % ss
-\usepackage{courier} % tt
-\usepackage{mathpazo} % math & rm
-\linespread{1.05}        % Palatino needs more leading (space between lines)
-\normalfont
-\usepackage[T1]{fontenc}
-''',
-}
-
-# Grouping the document tree into LaTeX files. List of tuples
-# (source start file, target name, title, author, documentclass [howto/manual]).
-latex_documents = [
-    (
-        "index",
-        "librosa.tex",
-        u"librosa Documentation",
-        u"The librosa development team",
-        "manual",
-    )
-]
-latex_engine = 'xelatex'
-# The name of an image file (relative to this directory) to place at the top of
-# the title page.
-latex_logo = 'img/BMW.svg'
-
-# For "manual" documents, if this is true, then toplevel headings are parts,
-# not chapters.
-# latex_use_parts = False
-
-# If true, show page references after internal links.
-# latex_show_pagerefs = False
-
-# If true, show URL addresses after external links.
-# latex_show_urls = False
-
-# Documents to append as an appendix to all manuals.
-# latex_appendices = []
-
-# If false, no module index is generated.
-# latex_domain_indices = True
 
 
 
